f19b2fdc753e18284b07abdf1343f1e16aabc815
soji.inohara <v004vk@gmail.com>
2019-10-12 09:56:30 +0000

    Revert "add column"
    This reverts commit f879230882a543d3fb20bde3457a4107fec815c9.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_092615) do
+ActiveRecord::Schema.define(version: 2019_10_11_134624) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
