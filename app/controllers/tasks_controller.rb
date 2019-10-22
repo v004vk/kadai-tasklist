98f61b3414eccfd702e6bcad3824aa962f166372
soji.inohara <v004vk@gmail.com>
2019-10-22 09:36:16 +0000

    update before_action in TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :new, :edit, :show]
-  before_action :set_task, only: [:edit, :show, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :show, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
@@ -45,8 +45,12 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      flash[:danger] = '指定タスクの閲覧権限がありません。'
+      redirect_to root_url
+    end
   end
   
   def task_params
